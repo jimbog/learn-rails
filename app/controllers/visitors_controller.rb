ab113dcc62f8985f6efdd9e164dabe6c51debf03
jimbog <jimmygarzon@gmail.com>
2013-11-28 18:45:19 -0500

    add some debug messages

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,9 @@
 class VisitorsController < ApplicationController
 
 	def new
+		#Rails.logger.debug 'DEBUG: entering new method'
 		@owner = Owner.new
+		#Rails.logger.debug 'DEBUG: owner name is ' + @owner.name
+		#raise 'deliberate Failure'
 	end
 end
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -8,6 +8,7 @@ class Owner
 	end
 
 	def countdown
+		#Rails.logger.debug 'DEBUG: entering Owner countdown method'
 		today = Date.today
 		birthday = Date.new(today.year, birthdate.month, birthdate.day)
 		if birthday > today
@@ -15,5 +16,6 @@ class Owner
 		else
 			countdown = (birthday.next_year - today).to_i
 		end
+
 	end
 end
