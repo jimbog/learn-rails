2c6330d1638a6e46af1c716c73ac8055cfd46921
jimbog <jimmygarzon@gmail.com>
2013-11-29 10:11:08 -0500

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,6 +3,9 @@ class VisitorsController < ApplicationController
 	def new
 		#Rails.logger.debug 'DEBUG: entering new method'
 		@owner = Owner.new
+		flash[:notice] = 'Welcome!'
+		flash[:alert] = 'My birthday is soon.'
+		
 		#Rails.logger.debug 'DEBUG: owner name is ' + @owner.name
 		#raise 'deliberate Failure'
 	end
