db53a03e4b3f493cb0a6ed2e867d8c07adcca6dd
jimbog <jimmygarzon@gmail.com>
2013-11-29 11:57:17 -0500

    front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,8 +3,9 @@ class VisitorsController < ApplicationController
 	def new
 		#Rails.logger.debug 'DEBUG: entering new method'
 		@owner = Owner.new
-		flash[:notice] = 'Welcome!'
-		flash[:alert] = 'My birthday is soon.'
+		#flash[:notice] = 'Welcome!'
+		#flash[:alert] = 'My birthday is soon.'
+		#flash[:warning] = 'A tornado!'
 		
 		#Rails.logger.debug 'DEBUG: owner name is ' + @owner.name
 		#raise 'deliberate Failure'
